d2d9aca35c3e6704f6b18b9e63e39d5308aab7f6
Jesse Malotaux <jemx@mboutrecht.nl>
2025-02-25 21:03:03 +0100

    Initial commit

---
+++ fe/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+
+console.log('app.vue loaded')
+</script>
+
+<style lang="scss" scoped></style>
---
+++ fe/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <img src="../assets/logo.svg" alt="" />
+    <h1>Home</h1>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
