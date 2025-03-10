c6346d89b47533f57eff27a6e46ed0dfc7831f1f
Jesse Malotaux <jemx@mboutrecht.nl>
2025-03-10 12:55:37 +0100

    New build.

---
+++ public/assets/PanelsView-CAFaSSDA.js
@@ -0,0 +1,2 @@
+import{_ as e,c,o as n}from"./index-CofyC8dZ.js";const o={};function r(t,s){return n(),c("div")}const _=e(o,[["render",r]]);export{_ as default};
+//# sourceMappingURL=PanelsView-CAFaSSDA.js.map
