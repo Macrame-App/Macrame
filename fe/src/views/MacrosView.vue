6b2402a7d73207e610d232dc66a13fa4c7398e4e
Jesse Malotaux <jemx@mboutrecht.nl>
2025-03-10 12:54:04 +0100

    Macros View, currently only occupied by the Macro Recorder.

--- fe/src/views/MacrosView.vue
+++ fe/src/views/MacrosView.vue
@@ -1,9 +1,31 @@
 <template>
-  <div></div>
+  <div id="macros" class="panel">
+    <h1>Macros</h1>
+    <MacroRecorder />
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import MacroRecorder from '../components/macros/MacroRecorder.vue'
+import { onMounted, ref } from 'vue'
+
+const recordMacro = ref(false)
+
+const macroInput = ref(null)
+
+onMounted(() => {
+  // macroInput.value.focus()
+})
+
+const keyDown = (e) => {
+  console.log(e)
+}
+</script>
 
 <style scoped>
 @reference "@/assets/main.css";
+
+input {
+  @apply border rounded-md bg-white/10 p-2;
+}
 </style>
