8c346edc59e745565c798e47914ea2614b32186c
Jesse Malotaux <jemx@mboutrecht.nl>
2025-03-23 18:14:03 +0100

    update public/

--- public/assets/SettingsView-BYnrwL7-.js
+++
@@ -1,2 +0,0 @@
-import{_ as e,c,o as t}from"./index-CofyC8dZ.js";const n={};function o(r,s){return t(),c("div")}const a=e(n,[["render",o]]);export{a as default};
-//# sourceMappingURL=SettingsView-BYnrwL7-.js.map
