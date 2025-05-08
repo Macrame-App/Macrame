2fe1266c34764d3a0f0997adad923bf55afbc2f3
JaxxMoss <j.d.malotaux@gmail.com>
2025-05-09 00:15:54 +0200

    Refactor build-scripts: directory added, old file deleted and windows-build file created.

---
+++ build-scripts/windows-build.bat
@@ -0,0 +1,51 @@
+@echo off
+
+REM Step 1: Build Macrame Go application for Windows
+echo Building Macrame Go Application for Windows...
+
+go build -ldflags "-H=windowsgui" -o Macrame.exe main.go
+
+IF %ERRORLEVEL% NEQ 0 (
+  echo Go build failed!
+  exit /b %ERRORLEVEL%
+) ELSE (
+  echo Go build was succesful! 
+)
+
+REM Step 2: Build Macrame Vue UI
+echo Moving to ui directory and building Vue UI
+cd ui
+
+echo Running npm install...
+call npm install
+IF %ERRORLEVEL% NEQ 0 (
+    echo npm install failed!
+    exit /b %ERRORLEVEL%
+)
+
+echo Running npm run build...
+call npm run build
+IF %ERRORLEVEL% NEQ 0 (
+    echo npm run build failed!
+    exit /b %ERRORLEVEL%
+)
+
+REM Step 3: Cleanup root directory of build files. 
+echo Cleaning up root directory...
+
+echo Removing directories: app, ui and build-scripts
+rmdir /s /q app
+rmdir /s /q ui
+rmdir /s /q build-scripts
+
+for %%F in (*) do (
+    set "file=%%~nxF"
+    if /I not "!file!"=="Macrame.exe" if /I not "!file!"=="favicon.ico" if /I not "!file!"=="README.md" (
+        echo Deleting !file!
+        del /f /q "%%F"
+    )
+)
+
+echo Build and cleanup complete. 
+
+
