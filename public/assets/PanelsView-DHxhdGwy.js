9a961079cd1147dfd27bf901a1f1dcf21e17087e
JaxxMoss <j.d.malotaux@gmail.com>
2025-05-08 23:13:48 +0200

    .gitignore update: ignore contents of public directory on dev branches

--- public/assets/PanelsView-DHxhdGwy.js
+++
@@ -1,9 +0,0 @@
-import { _ as _export_sfc, c as createElementBlock, o as openBlock } from "./index-GNAKlyBz.js";
-const _sfc_main = {};
-function _sfc_render(_ctx, _cache) {
-  return openBlock(), createElementBlock("div");
-}
-const PanelsView = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
-export {
-  PanelsView as default
-};
